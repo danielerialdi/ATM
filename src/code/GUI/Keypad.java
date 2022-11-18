163017825f06041331bc44926b6d84d7159723a2
Matteo <mattegianni85@gmail.com>
2022-11-18 14:57:51 +0100

    Prima Modifica double --> Euro

--- src/code/GUI/Keypad.java
+++ src/code/GUI/Keypad.java
@@ -3,6 +3,7 @@ package GUI;
 // Represents the keypad of the ATM
 import java.util.Scanner; // program uses Scanner to obtain user input
 
+
 public class Keypad
 {
    private Scanner input; // reads data from the command line
